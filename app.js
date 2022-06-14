e5b43cb7d45674f9221bbdf0988b8aa65d66bc0e
AronPelgrim <aron.pelgrim@hva.nl>
2022-06-14 14:12:40 +0200

    add stuff

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ app.set('views', './views/pages')
 
 app.use(express.static('public'))
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', (req, res) => {
   res.render('index')
